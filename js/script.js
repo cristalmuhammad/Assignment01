25aacb217664e5ab49ae6f51a5ba1fc553384cb7
Cristal Muhammad <cristalmuhammad@gmail.com>
2022-02-23 15:35:33 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,150 @@
+// STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+/* 
+Numeric Literal Expression
+let gpa = 3.5;
+
+String Literal Expression
+let name = 'Cristal;
+
+Boolean Literal Expression
+let approved = true;
+
+Null Literal Expression
+let favoriteColor = null; 
+*/
+
+// STEP 3
+//COMPLEX/VARIABLE EXPRESSION
+// let yourName = (lastName, firstName);
+// let speedLimit = (mpg + mph);
+
+// STEP 4-declare but do not assign 9 variables for the following identifiers First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. 
+/* 
+let firstName, lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWeContactYou; 
+*/
+
+// STEP 5 assign values to 3 variables listed above
+//   firstName = 'Cristal';
+//  lastName = 'Muhammad'
+//  city = 'Los Angeles'
+// console.log(firstName, lastName, city)
+
+// STEP 6 create a variable. add a number and a string and display the coerced result in the browsers consult window
+// let firstName = 'Cristal';
+// let yearsInSanDiego = 11;
+//  let areaOfResidence = 'Ocean Beach';
+//  alert(firstName + ' ' + 'has been living in San Diego for' + ' ' + yearsInSanDiego + ' years, in the area of ' + areaOfResidence);
+
+// STEP 7 create two variables. 1. add a boolean and a string and display in console 2. add a number and a boolean display in console
+// Part 1
+// let carType = 'Beetle';
+// let favoriteCar= true;
+// console.log(carType, favoriteCar);
+// Part 2 
+// let hoursStudying = 20;
+// let hoursSleeping = false;
+// console.log(hoursStudying, hoursSleeping);
+
+// STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former." '; 
+// window.console.log(someString);
+
+// STEP 9 create variable that produces null value in console
+// let job = 'full time';
+// job = null;
+// console.log(job);
+
+// STEP 10
+// string
+// let favoriteBand = String(alert('My Morning Jacket'));
+// console.log(typeof favoriteBand);
+
+// number
+// let planets = Number (8, 9)
+// console.log(typeof planets )
+
+// Boolean
+// let yearsInAge = Boolean(18);
+// let yearsAlive = Boolean(18);
+// console.log(typeof true);
+
+// object
+// let yourName = prompt('Cristal')
+// console.log(typeof yourName)
+
+// undefinded 
+// let favoriteColor;
+// console.log(typeof favoriteColor)
+
+// STEP 11 
+// let firstName = 'Zak';
+// let lastName = 'Ruvalcaba';
+// alert('Hello' +' '+ firstName + ' ' + lastName +', welcome to the JavaScript class!')
+
+// STEP 12
+//  let name = 'Cristal';
+//  alert('Hello' +' '+ name + ' ' + ', welcome to the JavaScript class!')
+
+// STEP 13
+//  let firstName = 'Cristal';
+//  let course = 'JavaScript'
+//  alert('Hello' +' '+ firstName + ' ' + ', welcome to the' + course + ' '+ 'class!')
+
+// STEP 14
+// let firstName = 'Zak';
+// let lastName = 'Ruvalcaba';
+// let course = 'Javascript';
+// alert('Hello' +' '+ firstName + ' ' + lastName +'. \nWelcome to the' + course +' '+ 'class!')
+
+// STEP 15 	Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable. 
+//  let name = prompt('Enter your name');
+//  prompt('Hello' +' '+ name + ' ' + ', welcome to the JavaScript class!')
+
+// STEP 16 Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
+//  let course = prompt('Enter your class name');
+//  prompt( 'Welcome to the' +' '+ course + ' '+ 'class!');
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// let sum = (x +y);
+//  console.log(sum);
+
+// STEP 18
+// let x = 20;
+// let sum = (x + 20)
+// console.log(sum)
+
+// STEP 19
+// let x = 20;
+// let sum = (x * 5)
+// console.log(sum)
+
+// STEP 20
+// let x = 20 % 3;
+// console.log(x)
+
+// STEP 21 Comparison and Logical Operators- write an application that evaluates to true and displays result in console
+// let x = 20
+// let y = 10
+// alert(x == 20 || y == 10)
+
+// STEP 22 Comparison and Logical Operators- using different operators write an application the evaluates to false, diplay in console. 
+// let x = 5;
+// let y = 10;
+// alert (x > y)
+
